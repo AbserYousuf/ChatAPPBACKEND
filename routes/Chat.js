081a2c4c417fbd7010518bee5f7cba20d2253541
Abser yousuf <abseryousuf50@gmail.com>
2026-09-02 20:58:04 +0530

    Fix image upload path in Chat.js

--- routes/Chat.js
+++ routes/Chat.js
@@ -9,9 +9,9 @@ const path = require('path')
 const { rateLimit } = require('express-rate-limit')
 const { body, validationResult } = require("express-validator");
 const storage = multer.diskStorage({
-   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, 'imageUploads'))
-},
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, '../imageuploads'))
+}
     filename: (req, file, cb) => {
         const uniqueName = `${req.userId}-${Date.now()}${path.extname(file.originalname)}`
         cb(null, uniqueName)
